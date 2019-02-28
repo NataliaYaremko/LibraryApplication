31051bb4d8f32281d921f4672e7f355d8c5d191c
unknown <nataliakinash@gmail.com>
2019-02-28 08:39:05 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
+
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+const books = [
+  {
+    title: 'The Wind in the Willows',
+    genre: 'Fantasy',
+    author: 'Kenneth Grahame',
+    read: false
+  },
+  {
+    title: 'The Wind in the Willows',
+    genre: 'History',
+    author: 'Kenneth Grahame',
+    read: false
+  },
+  {
+    title: 'The Wind',
+    genre: 'Biography',
+    author: 'Mark Twain',
+    read: false
+  },
+  {
+    title: 'The Wind in the Willows',
+    genre: 'Fantasy',
+    author: 'Kenneth Grahame',
+    read: false
+  },
+];
+bookRouter.route('/')
+  .get((req, res) => {
+    res.render(
+      'books',
+      {
+        nav: [{ link: '/books', title: 'Books' },
+          { link: '/authors', title: 'Authors' }],
+        title: 'Library',
+        books
+      },
+    );
+  });
+bookRouter.route('/single')
+  .get((req, res) => {
+    res.send('hello single book');
+  });
+
+app.use('/books', bookRouter);
+app.get('/', (req, res) => {
+  res.render(
+    'index',
+    {
+      nav: [{ link: '/books', title: 'Books' },
+        { link: '/authors', title: 'Authors' }],
+      title: 'Library'
+    }
+  );
+});
+
+app.listen(port, () => {
+  debug(`Listening on port ${chalk.green(port)}`);
+});
